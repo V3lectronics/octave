b16b8b918560ad37fa47fb84159acde13c326e51
barbarjan <niszczyciel.agawa@gmail.com>
2025-11-01 12:57:09 +0100

    add data

--- pw-octave/mcda.m
+++ pw-octave/mcda.m
@@ -1,13 +1,20 @@
 parametry = [
+%auto   1     2     3     4
 	50000 30000 45000 10000; %cena
-	1     3     5     9    ; %wyglad
-	4     7     7     8    ; %naprawialnosc
+	1     3     5     9    ; %wyglad 1-10
+	4     7     7     3    ; %naprawialnosc 1-10
+	5     5     2     7    ; %ilość miejsc
+	50    100   200   90  ; %KM
+	30    50    60    30; %bak
 	]
 
 wagi = [
 	0.8; %cena
 	0.3; %wyglad
 	0.5; %naprawialnosc
+	0.5; %ilość miejsc
+	0.6; %KM
+	0.3; %bak
 ]
 
 format short
@@ -22,8 +29,7 @@ function retval = invert(v)
 	retval = v.^(-1);
 endfunction
 
-
-parametry(1,:) = mmnormalize(parametry(1,:), 0);
+% parametry(1,:) = mmnormalize(parametry(1,:), 0)
 parametry(1,:) = invert(parametry(1,:));
 
 
