4a0180dca598d40d00c4bb72553570d9be988342
barbarjan <niszczyciel.agawa@gmail.com>
2025-11-01 13:15:17 +0100

    add przebieg

--- pw-octave/mcda.m
+++ pw-octave/mcda.m
@@ -1,11 +1,12 @@
 parametry = [
 %auto   1     2     3     4
-	50000 30000 45000 10000; %cena
-	1     3     5     9    ; %wyglad 1-10
-	4     7     7     3    ; %naprawialnosc 1-10
-	5     5     2     7    ; %ilość miejsc
-	50    100   200   90   ; %KM
-	30    50    60    30   ; %bak
+	50000 30000 45000 10000   ; %cena
+	1     3     5     9       ; %wyglad 1-10
+	4     7     7     3       ; %naprawialnosc 1-10
+	5     5     2     7       ; %ilość miejsc
+	50    100   200   90      ; %KM
+	30    50    60    30      ; %bak
+	50000 100000 300000 120000; %przebieg
 	]
 
 wagi = [
@@ -15,6 +16,7 @@ wagi = [
 	0.5; %ilość miejsc
 	0.6; %KM
 	0.3; %bak
+	0.7; %przebieg
 ]
 
 %=============================================================
@@ -35,6 +37,7 @@ endfunction
 
 % parametry(1,:) = mmnormalize(parametry(1,:), 0)
 parametry(1,:) = invert(parametry(1,:));
+parametry(7,:) = invert(parametry(7,:));
 
 
 for i = [1: size(parametry)(1) ]
